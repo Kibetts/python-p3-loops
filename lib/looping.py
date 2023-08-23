af02954e264a0a0e17f3f880d3c42a04e8d42fc9
Brian Kibet <kibetsbrian@gmail.com>
2023-08-23 22:15:18 +0300

    Completed assignments

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    for i in range(10,0):
+        if i == 1:
+            print("Happy New Year!")
+    print(i)
+   
+happy_new_year()
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    for item  in int_list:
+        return item * item
+
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+    
